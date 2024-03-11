3d6e3c8df1d321ea85da0c19ce97ebe8d88f2d64
mqxu <moqi1977@gmail.com>
2024-03-11 14:05:45 +0800

    spring-boot-quickstart 模块

--- spring-boot-quickstart/src/main/java/top/mqxu/boot/quickstart/QuickStartApplication.java
+++ spring-boot-quickstart/src/main/java/top/mqxu/boot/quickstart/QuickStartApplication.java
@@ -1,7 +1,22 @@
-package top.mqxu.boot.quickstart;/**
-  *@author mqxu
-  *@date 2024/3/11
-  *@description QuickStartApplication
-**/
+package top.mqxu.boot.quickstart;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author mqxu
+ * @date 2024/3/11
+ * @description QuickStartApplication
+ **/
+@Slf4j
+@SpringBootApplication
 public class QuickStartApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(QuickStartApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("启动成功");
+        log.info("测试地址：http://localhost:{}/hello", env.getProperty("server.port"));
+    }
 }
